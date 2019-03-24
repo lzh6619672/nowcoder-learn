0d3382ad3e605aad35103c801bab8119e8b93ea4
leo <lzh_lxf@qq.com>
2019-03-24 16:37:12 +0800

    write your meaaage

---
+++ Solution.java
@@ -0,0 +1,27 @@
+package io.github.jiangdequan;
+import java.util.ArrayList;
+
+public class Solution {
+    private static ArrayList<Integer> list = new ArrayList<>();
+
+    public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        if (null != listNode && null != listNode.next) {
+            printListFromTailToHead(listNode.next);
+            list.add(listNode.next.val);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        printListFromTailToHead(null);
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
